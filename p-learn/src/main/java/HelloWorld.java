f379fafd690317f44d80fa5fdf446033078433d1
qinwanxin <qinwanxin@jd.com>
2018-05-13 14:18:24 +0800

    init

--- p-learn/src/main/java/HelloWorld.java
+++ p-learn/src/main/java/HelloWorld.java
@@ -3,6 +3,15 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello test");
+        testJava8();
+    }
+
+    public static void testJava8(){
+        /**
+         *  () -> {} replace no named inner class
+         */
+        new Thread( () -> System.out.println("hello java8")).start();
     }
 }
