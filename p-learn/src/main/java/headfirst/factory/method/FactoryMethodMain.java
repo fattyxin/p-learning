b48cf743b406dab166e5a7a7aa8d5daa381c4c6d
qw844460848@163.com <qw844460848@163.com>
2018-05-19 17:04:25 +0800

    简单工厂

---
+++ p-learn/src/main/java/headfirst/factory/method/FactoryMethodMain.java
@@ -0,0 +1,12 @@
+package headfirst.factory.method;
+
+public class FactoryMethodMain {
+    /**
+     *
+     * 工厂方法模式
+     * 工厂方法模式通过让子类决定该创建的对象是什么，来达到将对象创建的过程封装的目的。
+     *
+     * 有两家蛋糕店 好利来：Holiland  米旗:maky
+     * 都有 奶油蛋糕、水果蛋糕
+     */
+}
